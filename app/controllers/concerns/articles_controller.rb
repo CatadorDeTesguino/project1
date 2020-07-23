2311d4b82a326eee383c863fe3de8945e8f091a6
CatadorDeTesguino <66817382+CatadorDeTesguino@users.noreply.github.com>
2020-07-22 22:00:44 -0600

    front end creacion de articulos

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -2,12 +2,15 @@ class ArticlesController <ApplicationController
     def show
         @article = Article.find(params[:id])   
     end
+
     def index
         @articles = Article.all
     end
-    def def new
+
+    def new
     end
-    end
-    def def create
+
+    def create
+        render plain: params[:article]
     end
 end
