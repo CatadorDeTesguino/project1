5731733ced0a10dd06d2d0a5886c91a3f1936894
CatadorDeTesguino <66817382+CatadorDeTesguino@users.noreply.github.com>
2020-07-23 20:50:54 -0600

    Post Edit added

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -7,10 +7,34 @@ class ArticlesController <ApplicationController
         @articles = Article.all
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def new
+        @article =Article.new
     end
 
     def create
-        render plain: params[:article]
+        @artilce = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to @article #article_path(@article)
+        else 
+            render 'new'
+        
+        end
     end
+
+    def update
+        @article = Aritcle.find(params[id])
+        if @artice.update(params.require(:article).permit(:title :description))
+            flash[:notice] = "Article was updated succesfully"
+            redirect_to @article #Por que redirec 
+        else 
+            render 'edit'
+        end
+    end
+
+
 end
